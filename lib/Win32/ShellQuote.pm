354bf3406a82de1423be50de9c100605b32b6b4d
Graham Knop <haarg@haarg.org>
2012-03-28 14:45:22 -0400

    pod

--- lib/Win32/ShellQuote.pm
+++ lib/Win32/ShellQuote.pm
@@ -131,3 +131,93 @@ sub _has_shell_metachars {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Win32::ShellQuote - Quote argument lists for Win32
+
+=head1 SYNOPSIS
+
+    use Win32::ShellQuote qw(:all);
+
+    system quote_system('program.exe', '--switch', 'argument with spaces or other special characters');
+
+=head1 DESCRIPTION
+
+Quotes argument lists to be used in Win32 in a several different
+situations.
+
+Windows passes its arguments as a single string instead of an array
+as other platforms do.  In almost all cases, the standard Win32
+L<CommandLineToArgvW|http://msdn.microsoft.com/en-us/library/ms647232.aspx>
+function is used to parse this string.  F<cmd.exe> has different
+rules for handling quoting, so extra work has to be done if it is
+involved.  It isn't possible to consistantly create a single string
+that will be handled the same by F<cmd.exe> and the stardard parsing
+rules.
+
+Perl will try to detect if you need the shell by detecting shell
+metacharacters.  The routine that checks that uses different quoting
+rules from both F<cmd.exe> and the native Win32 parsing.  Extra
+work must therefore be done to protect against this autodetection.
+
+=head1 SUBROUTINES
+
+=head2 quote_native
+
+Quotes as a string to pass directly to a program using native methods
+like L<Win32::Spawn()|Win32>.  This is the safest option to use if
+possible.
+
+=head2 quote_cmd
+
+Quotes as a string to be run through cmd.exe, such as in a batch file.
+
+=head2 quote_system_list
+
+Quotes as a list to be passed to L<system|perlfunc/system> or
+L<exec|perlfunc/exec>.  This is equally as safe as L</quote_native>,
+but you must ensure you have more than one item being quoted for
+the list to be usable with system.
+
+=head2 quote_system_string
+
+Like L</quote_system_list>, but returns a single string.  Some
+argument lists cannot be properly quoted using this function.
+
+=head2 quote_system
+
+Switches between L</quote_system_list> and L</quote_system_string>
+based on the number of items quoted.
+
+=head2 quote_system_cmd
+
+Quotes as a single string that will always be run with F<cmd.exe>
+
+=head2 quote_literal
+
+Quotes a single parameter in native form.
+
+=head2 cmd_escape
+
+Escapes a string to be passed untouched by F<cmd.exe>.
+
+=head1 CAVEATS
+
+=over
+
+=item *
+
+Newlines (\n or \r) can't be properly quoted when running through F<cmd.exe>.
+
+=item *
+
+Some functions rely on undocumented parts of the perl internals
+that have been re-implemented in this module.
+
+=back
+
+=cut
+
